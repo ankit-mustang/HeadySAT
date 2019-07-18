8cb07c4fdca68f484f6575674953b18bc3d227b5
ankit-mustang <53053332+ankit-mustang@users.noreply.github.com>
2019-07-18 22:05:06 +0530

    Add files via upload

---
+++ HeadySAT/Script.js
@@ -0,0 +1,44 @@
+﻿
+var headyApp = angular.module("headyApp", ["ngRoute"]);
+headyApp.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+    $locationProvider.html5mode(true).hashPrefix('!');
+    $routeProvider
+        .when("/details", {
+            templateUrl: "/Templates/details.htm",
+            controller: "detailsController"
+        })
+        .when("/description", {
+            templateUrl: "/Templates/description.htm",
+            controller: "descriptionController"
+        })
+        .when("/reviews", {
+            templateUrl: "/Templates/reviews.htm",
+            controller: "reviewsController"
+        })
+        .when("/related", {
+            templateUrl: "/Templates/related.htm",
+            controller: "relatedController"
+        })
+        .otherwise({
+            redirectTo:"/Templates/details.htm"
+        });
+}]);
+headyApp.controller("detailsController", function ($scope) {
+    //$scope.message = "I love Paris";
+});
+headyApp.controller("descriptionController", function ($scope) {
+    //$scope.message = "I love Paris";
+});
+headyApp.controller("reviewsController", function ($scope) {
+    $scope.message = "Reviews Page";
+});
+headyApp.controller("relatedController", function ($scope) {
+    $scope.message = "Related Page";
+});
+
+//var changeText = function(id) {
+//    var getText = document.getElementById(id).innerText;
+//    document.getElementById("verticalText").innerText = getText;
+//}
+
+
